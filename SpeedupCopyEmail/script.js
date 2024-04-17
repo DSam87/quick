22c46000d437dae0b8a06a2327d44d42d51f56bd
Sam Caballero <deleonsam87@gmail.com>
2024-04-17 08:54:01 -0500

    Add files via upload

---
+++ SpeedupCopyEmail/script.js
@@ -0,0 +1,85 @@
+function marFunction(){
+    navigator.clipboard.writeText(
+        "Member added to replacements tab. \n" + "Member should receive card within 30 days. \n" + "Thank you so much and hope you are having a great day!" 
+    );
+}
+
+function mamFunction(){
+    navigator.clipboard.writeText(
+        "Member added to master tab. \n" + "Member should receive card within 4 to 6 weeks.\n" + "Thank you so much and hope you are having a great day!"
+    );
+}
+
+function mapFunction(){
+    navigator.clipboard.writeText(
+        "Member was added on _____ and remail date _____ \n\n" + "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function mthFunction(){
+    navigator.clipboard.writeText(
+        "Based on the member's plan, the member has to contact their health insurance provider for any/if any rewards.\n\n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function mnfFunction(){
+    navigator.clipboard.writeText(
+        "Could not find member with signify health. \n\n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function mndFunction(){
+    navigator.clipboard.writeText(
+        "Member has no dos with signify health. \n \n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function tmtFunction(){
+    navigator.clipboard.writeText(
+        "Member has a dos before 2023 \n" +
+        "Too much time has elapsed for member to receive rewards for any dos before 2023. \n \n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function naiFunction(){
+    navigator.clipboard.writeText(
+        "Please get full verified address from member for gift card emails.\n" +
+        "Member lives in an Apartment/Unit.\n\n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function caFunction(){
+    navigator.clipboard.writeText(
+        "Conflicting 2.0 address and provided address. \n" +
+        "Was your provided address verified by member? \n \n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function ndosFunction(){
+    navigator.clipboard.writeText(
+        "Please add dos. \n" +
+        "Member has both 2023 and 2024 dos. \n \n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function giFunction(){
+    navigator.clipboard.writeText(
+        "Mbrs must use gcs in store and use as credit/gc. Most gcs have no pin number. Mbrs must tell cashier its a gift card or credit.  \n\n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
+function gaFunction(){
+    navigator.clipboard.writeText(
+        "For info about gc that need activation, member would call the number on the back of the card or go to myprepaidcenter.com for activation.  \n\n" +
+        "Thank you so much and hope you are having a great day! \n"
+    );
+}
+
